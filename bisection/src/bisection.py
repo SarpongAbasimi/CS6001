e9c737a1f9545e789b4bf39131a64fcca22c5d8e
sarpong <abasimisarpong@rocketmail.com>
2019-07-02 11:20:15 +0100

    wrote test to check if computer guesses the right number

--- bisection/src/bisection.py
+++ bisection/src/bisection.py
@@ -3,3 +3,13 @@ class Bisection():
     self.low = 0 
     self.high = 10 
     self.answer = (self.low + self.high)/ 2
+
+  def guess(self, number):
+    if type(number) is str:
+      return []
+    while True:
+      if self.answer == number:
+        print(f'your number was {self.answer}')
+        return number
+        break
+    
--- bisection/tests/bisection_test.py
+++ bisection/tests/bisection_test.py
@@ -15,3 +15,16 @@ class TestBisection(object):
     assert bisection.low == 0
     assert bisection.high == 10
     assert bisection.answer == (bisection.low + bisection.high)/ 2
+  
+  def test_guess(self, bisection):
+    number = 5
+    assert bisection.guess(number) == bisection.answer
+  
+  def test_guess_returns_list_if_passed_string(self, bisection):
+    number = 's'
+    assert bisection.guess('s') == []
+  
+  def test_guess_prints_what_user_input(self, bisection, capsys):
+    bisection.guess(5)
+    output = capsys.readouterr()
+    assert output.out == f'your number was {bisection.answer}\n'
